e0abba00c90146ea70858ce869a33d481c3d3f89
jadedec <106964384+jadedec@users.noreply.github.com>
2022-07-28 09:53:07 +0100

    react and ghPage setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+
+
+//TODO: DO REMEMBER CHANGE REPO NAME IN PACKAGE.JSON
+
+
+
+const App = () => {
+  return (
+    <>
+      <p>react works</p>
+    </>
+  );
+};
+
+export default App;
